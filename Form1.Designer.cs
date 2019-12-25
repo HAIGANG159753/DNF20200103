b4809d2a7a9719380c8bd374a246ed12dee00fdb
HAIGANG <771262509@qq.com>
2019-12-26 00:38:31 +0800

    201912260038

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DNFtest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startgametest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startgametest
+            // 
+            this.startgametest.Location = new System.Drawing.Point(44, 46);
+            this.startgametest.Name = "startgametest";
+            this.startgametest.Size = new System.Drawing.Size(168, 96);
+            this.startgametest.TabIndex = 0;
+            this.startgametest.Text = "进行一些操作";
+            this.startgametest.UseVisualStyleBackColor = true;
+            this.startgametest.Click += new System.EventHandler(this.startgametest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.startgametest);
+            this.Name = "Form1";
+            this.Text = "测试用";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startgametest;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Lordeo.Framework;
+
+namespace DNFtest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startgametest_Click(object sender, EventArgs e)
+        {
+            //Bitmap bitmap = publicmethod.GetScreenCapture();
+            //bitmap.Save(@"G:\\Game\\VSpicture\\NewDriverScreen.jpg");
+            IntPtr hWnd = IntPtr.Zero;
+            //Console.WriteLine(1);
+            //Console.WriteLine(hWnd);
+            string savePath = "D:\\temp.bmp";
+            var processes = Process.GetProcessById(9936);
+            //Console.WriteLine(processes);
+            var title = processes.MainWindowTitle;
+            hWnd = Win32API.FindWindow(null, title);//得到名称为“记事本”的窗口句柄。
+            //Console.WriteLine(2);
+            //Console.WriteLine(hWnd);
+            if (hWnd.ToString() == "0")
+            {
+                MessageBox.Show("未能找到目标窗口");
+            }
+            else
+            {
+
+                //savePath = "D\\temp.bmp";//设置图片的临时保存路径。
+                //RECT rect = new RECT();
+                //Win32API.GetWindowRect(hWnd, ref rect);
+
+                Bitmap img = publicmethod.GetImg(hWnd);//X,Y为所要获取截图的窗口宽度和高度。
+
+                img.Save(savePath);//保存得到的截图。
+                MessageBox.Show("截图成功");
+            }
+        }
+
+        
+
+    }
+}
